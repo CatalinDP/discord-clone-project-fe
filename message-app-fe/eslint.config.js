ecd0ff0fdadd226fa16f01fcbd4fec27e1682b39
CatalinDP <DPCatalin@hotmail.com>
2026-06-13 14:23:17 +0200

    project created and initialized - configured

---
+++ message-app-fe/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier' // ◄ 1. Importar
+import prettierPlugin from 'eslint-plugin-prettier'   // ◄ 2. Importar
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      fontSize: 20,
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'prettier': prettierPlugin, // ◄ 3. Añadir plugin
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // ◄ 4. Mostrar fallos de formato como errores
+    },
+  },
+  prettierConfig // ◄ 5. Desactivar reglas que choquen con Prettier
+)
