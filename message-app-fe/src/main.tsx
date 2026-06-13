098268aa76b875b0f65156c8008323af6d86f1d4
CatalinDP <DPCatalin@hotmail.com>
2026-06-13 18:41:56 +0200

    add todo for test

--- message-app-fe/src/main.tsx
+++ message-app-fe/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+//TODO: add react-router
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
